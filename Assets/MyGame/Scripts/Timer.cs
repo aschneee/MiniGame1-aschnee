e3e5feb1a40e58c81279c2f717bc74f59c985336
aschnee <alina.schneeberger@htl-salzburg.ac.at>
2019-06-01 13:13:36 +0200

    added timer and switch to endscene

---
+++ Assets/MyGame/Scripts/Timer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour {
+
+    private float time;
+
+    public TextMeshProUGUI timeText;
+    public float maxTime = 60f;
+    public string deathScene = "EndScene";
+
+
+
+	// Use this for initialization
+	void Start () {
+        time = maxTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        timeText.text = time.ToString("F");
+
+        if (time > 0)
+        {
+            time -= Time.deltaTime;
+        }
+        else if (time <= 0)
+        {
+            SceneManager.LoadScene(deathScene);
+        }
+
+	}
+}
