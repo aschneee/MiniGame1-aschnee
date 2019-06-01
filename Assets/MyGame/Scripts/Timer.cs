16f15ad133682732e91da6780d77cadf5436c75c
aschnee <alina.schneeberger@htl-salzburg.ac.at>
2019-06-01 17:50:53 +0200

    cleaned script and beautified timer script

--- Assets/MyGame/Scripts/Timer.cs
+++ Assets/MyGame/Scripts/Timer.cs
@@ -4,24 +4,23 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
 
-public class Timer : MonoBehaviour {
-
+public class Timer : MonoBehaviour
+{
     private float time;
 
     public TextMeshProUGUI timeText;
     public float maxTime = 60f;
     public string deathScene = "EndScene";
 
-
-
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
         time = maxTime;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-
+	void Update ()
+    {
         timeText.text = time.ToString("F");
 
         if (time > 0)
